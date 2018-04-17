6be0e4535c456650bf563eda014857eb108b9b6a
dannfielding <3field08@solent.ac.uk>
2018-04-17 14:27:13 +0000

    lol

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+function main() {
+    var slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        var i;
+        var slides = document.getElementsByClassName("pictures");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) { slideIndex = 1 }
+        slides[slideIndex - 1].style.display = "block";
+        setTimeout(showSlides, 10000); // 7 seconds between pictures changing
+    }
+
+}
