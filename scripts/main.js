0fbf52f02d154f8a171378d093403d8d0a4c52c6
dannfielding <3field08@solent.ac.uk>
2018-04-18 12:38:10 +0000

    Caption for Carousel

--- scripts/main.js
+++ scripts/main.js
@@ -14,4 +14,33 @@ function main() {
         setTimeout(showSlides, 10000); // 7 seconds between pictures changing
     }
 
+    function clock() {
+
+        var today = new Date();
+        var curr_hour = today.getHours();
+        var curr_minute = today.getMinutes();
+        var curr_second = today.getSeconds();
+        var cur_time = curr_hour + ":" + curr_minute + ":" + curr_second;
+        console.log(cur_time);
+
+        if (curr_minute < 10)
+            curr_minute = "0" + curr_minute;
+
+        var suffix = "AM";
+        if (curr_hour >= 12) {
+            suffix = "PM";
+            curr_hour = curr_hour - 12;
+        }
+        if (curr_hour == 0) {
+            curr_hour = 12;
+        }
+
+        var cur_time = curr_hour + ":" + curr_minute + ":" + curr_second + " " + suffix;
+
+
+
+        function startTime() {
+            setInterval(main, 500);
+        }
+    }
 }
