644d3033c44c87348623f8d3101f513ad891fe93
dannfielding <3field08@solent.ac.uk>
2018-05-09 11:10:28 +0000

    Contact Us Form Validation Text

--- scripts/main.js
+++ scripts/main.js
@@ -21,3 +21,31 @@ function main() {
         document.getElementById("time").innerHTML = d.toLocaleTimeString();
     }
 }
+
+function contactValidation() {
+    var first_name = document.contactform.first_name.value;
+    var last_name = document.contactform.last_name.value;
+    var email = document.contactform.email.value;
+    var phone = document.contactform.phone.value;
+    
+    if ((first_name == null) || (first_name == "")) {
+        document.getElementById("first_name_error").innerHTML = "Please enter a first name.";
+        return false;
+    }
+    if ((last_name == null) || (last_name == "")) {
+        document.getElementById("last_name_error").innerHTML = "Please enter a last name.";
+        return false;
+    }
+
+    if ((email == null) || (email == "")) {
+        document.getElementById("email_error").innerHTML = "Please enter a email address.";
+        return false;
+    }
+    
+    if (isNaN(phone)){
+        document.getElementById("phone_error").innerHTML = "Please enter a phone number.";
+        return false;
+    }
+    
+    
+}
