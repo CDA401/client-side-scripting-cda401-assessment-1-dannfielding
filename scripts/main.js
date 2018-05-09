c8d8f2aa208dbf1051f7a697e757a5ed231229d4
dannfielding <3field08@solent.ac.uk>
2018-05-09 14:10:49 +0000

    Real time validation on Contact Us Page

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 function main() {
     var slideIndex = 0;
     showSlides();
+    
+
 
     function showSlides() {
         var i;
@@ -25,33 +27,70 @@ function main() {
 
 
 function contactValidation() {
-    var first_name = document.forms["contactform"]["first_name"].value;
-    var last_name = document.forms["contactform"]["last_name"].value;
-    var email = document.forms["contactform"]["email"].value;
-    var phone = document.forms["contactform"]["phone"].value;
-    
-    if ((first_name == null) || (first_name == "")) {
+    var first_name = document.forms["contactform"]["fname"].value;
+    if (first_name == "") {
         document.getElementById("first_name_error").innerHTML = "Please enter a first name.";
         event.preventDefault();
-        return false;
-    }
-    if ((last_name == null) || (last_name == "")) {
-        document.getElementById("last_name_error").innerHTML = "Please enter a last name.";
-        event.preventDefault();
-        return false;
-    }
-
-    if ((email == null) || (email == "")) {
-        document.getElementById("email_error").innerHTML = "Please enter a email address.";
-        event.preventDefault();
-        return false;
     }
     
-    if ((phone == null) || (phone == "")) {
+    var last_name = document.forms["contactform"]["lname"].value;
+    if (last_name == "") {
+        document.getElementById("last_name_error").innerHTML = "Please enter a last name.";
+        event.preventDefault();
+    }
+
+    var email = document.forms["contactform"]["email"].value;
+    if (email == "") {
+        document.getElementById("email_error").innerHTML = "Please enter a email address.";
+        event.preventDefault();
+    }
+    
+    var phone = document.forms["contactform"]["phone"].value;
+    if (phone == "") {
         document.getElementById("phone_error").innerHTML = "Please enter a phone number.";
         event.preventDefault();
-        return false;
     }
     
 
 }
+
+function fnameValidation() {
+    var first_name = document.getElementById("fname");
+
+    if (first_name.value == "") {
+        first_name.style.backgroundColor = "red";
+       } else {
+            first_name.style.backgroundColor = "";
+        }
+    }
+
+function lnameValidation() {
+    var last_name = document.getElementById("lname");
+
+    if (last_name.value == "") {
+        last_name.style.backgroundColor = "red";
+       } else {
+            last_name.style.backgroundColor = "";
+        }
+    }
+    
+    function emailValidation() {
+    var email = document.getElementById("emailv");
+
+    if (email.value == "") {
+        email.style.backgroundColor = "red";
+       } else {
+            email.style.backgroundColor = "";
+        }
+    }
+    
+    function phoneValidation() {
+    var phone = document.getElementById("phonev");
+
+    if (phone.value == "") {
+        phone.style.backgroundColor = "red";
+       } else {
+            phone.style.backgroundColor = "";
+        }
+    }
+
