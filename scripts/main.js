44920c9b0ee99caa7954ecaa8e70704bb121ec7c
dannfielding <3field08@solent.ac.uk>
2018-05-10 16:17:35 +0000

    Order Page Total Calculation (Working but can be optimised)

--- scripts/main.js
+++ scripts/main.js
@@ -159,3 +159,71 @@ function postValidation() {
         postcode.style.backgroundColor = "";
     }
 }
+
+function realtimetotal() {
+    var items = new Array();
+    var totalprice = "0.00";
+    var output = "";
+    
+    var basetotal = 0;
+    var base_small = document.getElementById("base_small");
+    if (base_small.checked) {
+        basetotal = base_small.value;
+        items.push("Small Pizzabase - £5");
+    }
+    var base_medium = document.getElementById("base_medium");
+    if (base_medium.checked) {
+        basetotal = base_medium.value;
+        items.push("Medium Pizzabase - £7.50");
+    }
+    var base_large = document.getElementById("base_large");
+    if (base_large.checked) {
+        basetotal = base_large.value;
+        items.push("Large Pizzabase - £10");
+    }
+    var base_xlarge = document.getElementById("base_xlarge");
+    if (base_xlarge.checked) {
+        basetotal = base_xlarge.value;
+        items.push("X-Large Pizzabase - £12.50");
+    }
+    
+    var toppingtotal = 0;
+    var cheese = document.getElementById("cheese");
+    if (cheese.checked) {
+        toppingtotal = +toppingtotal + +cheese.value;
+        items.push("Cheese Topping - £0.20");
+    }
+    var mushrooms = document.getElementById("mushrooms");
+    if (mushrooms.checked) {
+        toppingtotal = +toppingtotal + +mushrooms.value;
+        items.push("Mushrooms Topping - £0.40");
+    }
+    var ham = document.getElementById("ham");
+    if (ham.checked) {
+        toppingtotal = +toppingtotal + +ham.value;
+        items.push("Ham Topping - £0.50")
+    }
+    var anchovies = document.getElementById("anchovies");
+    if (anchovies.checked) {
+        toppingtotal = +toppingtotal + +anchovies.value;
+    }
+    
+    var extratotal = 0;
+    var chips = document.getElementById("chips");
+    if (chips.checked) {
+        extratotal = +extratotal + +chips.value;
+    }
+    var garlic_bread = document.getElementById("garlic_bread");
+    if (garlic_bread.checked) {
+        extratotal = +extratotal + +garlic_bread.value;
+    }
+    
+    totalprice = +basetotal + +toppingtotal + +extratotal;
+    
+    totalprice = parseFloat(totalprice).toFixed(2);
+    
+    document.getElementById("totalprice").innerHTML = totalprice;
+    
+    
+    
+}
